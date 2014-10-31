cc7faa1b7ab8d3308264a48a47ae16fbbdec988e
Matthew Mosby <mmosby1@nd.edu>
2014-10-31 19:08:51 +0000

    add master command line option for execution environment
    Includes some debugging/error mesages. Valid options are -SS and -MS for Single Scale
    and Multi Scale respectively. This option must be passed as the first option. It takes 
    the first valid option

--- src/super_main.c
+++ src/super_main.c
@@ -10,24 +10,54 @@ int multi_scale_main(int argc, char **argv);
 
 
 /** define switching variables */
-enum{SINGLE_SCALE,MULTI_SCALE};
-
-#ifndef PGFEM_FRAMEWORK
-#define PGFEM_FRAMEWORK MULTI_SCALE
-#endif
+enum{SINGLE_SCALE,MULTI_SCALE,N_FRAMEWORK};
+static const char *framework_opts[] = {"-SS","-MS"};
 
 #include <stdio.h>
+#include <string.h>
+
+/**
+ * get the execution framework from the very first passed option.
+ *
+ * It is an error to not have any options or to pass an undefined
+ * option. \return non-zero on error.
+ */
+static void get_framework(int argc,
+			  char **argv,
+			  int *PGFEM_FRAMEWORK)
+{
+  *PGFEM_FRAMEWORK = N_FRAMEWORK; /* poison value */
+  if(argc < 2){
+    fprintf(stderr,"ERROR: must provide at least one option!\n");
+    return;
+  } else {
+    /* check for match agains the framework option strings */
+    for(int i=0; i<N_FRAMEWORK; i++){
+      if(!strcmp(argv[1],framework_opts[i])){
+	*PGFEM_FRAMEWORK = i;
+	break;
+      }
+    }
+  }
+}
+
+static void print_valid_usage(FILE *out)
+{
+  fprintf(out,"Required usage: PGFem3D [FRAMEWORK] {options}\n");
+  fprintf(out,"\tValid FRAMEWORK options: -SS (single scale) or -MS (multi-scale)\n\n");
+  fprintf(out,"Run \'PGFem3D [FRAMEWORK] -h\' for other option information.\n");
+}
 
 int main(int argc, char **argv)
 {
   int err = 0;
-
+  int PGFEM_FRAMEWORK = 0;
+  get_framework(argc, argv,&PGFEM_FRAMEWORK);
   switch(PGFEM_FRAMEWORK){
   case SINGLE_SCALE: err = single_scale_main(argc,argv); break;
   case MULTI_SCALE: err = multi_scale_main(argc,argv); break;
   default:
-    fprintf(stderr,"ERROR: Undefined PGFEM_FRAMEORK (%d)!\n\n",
-	    PGFEM_FRAMEWORK);
+    print_valid_usage(stderr);
     err = 1;
     break;
   }
