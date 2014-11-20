4d6a8f263b1083a0e59911c4ae5d8b12b33c0da5
Matthew Mosby <mmosby1@nd.edu>
2014-11-20 21:09:57 +0000

    Minor cleanup of header file

--- lib/VTK_IO/include/PGFem3D_to_VTK.hpp
+++ lib/VTK_IO/include/PGFem3D_to_VTK.hpp
@@ -1,38 +1,23 @@
 /* HEADER */
+#pragma once
 #ifndef _PGFEM_TO_VTK_HPP_
 #define _PGFEM_TO_VTK_HPP_
 
-#ifndef ELEMENT_H
-#include "element.h"
-#endif
-
-#ifndef NODE_H
-#include "node.h"
-#endif
-
-#ifndef SUPP_H
-#include "supp.h"
-#endif
-
-#ifndef SIG_H
-#include "sig.h"
-#endif
-
-#ifndef EPS_H
-#include "eps.h"
-#endif
-
-#ifndef ENUMERATIONS_H
-#include "enumerations.h"
-#endif
-
-#ifndef INTERFACE_MACRO_H
-#include "interface_macro.h"
-#endif
+// pre include mpi. This is necessary so that there is not a conflict
+// with c/c++ when wrapped with extern c
+#include "mpi.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* #ifdef __cplusplus */
+
+#include "element.h"
+#include "node.h"
+#include "supp.h"
+#include "sig.h"
+#include "eps.h"
+#include "enumerations.h"
+#include "interface_macro.h"
 
 /** Create a vtkUnstructuredGrid object from a PGFem3D mesh and
     associated information. ***CURRENTLY ONLY SUPPORT LINEAR TETRAS
