5f968704b4464f6a4fae7ea61f2fc63145c7c565
Matthew Mosby <mmosby1@nd.edu>
2014-11-24 16:19:39 +0000

    Implement data structure/functions for scanning the solver file.
    Self-contained solver file data structure. Used for updating boundary conditions
    and holding solver parameters. Created to homogenize interface among SS and MS
    frameworks and to aid in restart.

---
+++ include/solver_file.h
@@ -0,0 +1,80 @@
+/* HEADER */
+/**
+ * Declare functions for reading/manipulating the solver
+ * file. Introduce a structure for storing/updating the data. All
+ * functions return non-zero on (detected) error.
+ *
+ * AUTHORS:
+ *  Matt Mosby, University of Notre Dame <mmosby1@nd.edu>
+ */
+#pragma once
+#ifndef SOLVER_FILE_H
+#define SOLVER_FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct SOLVER_FILE {
+  FILE *file; /**< file to read data from */ 
+  double nonlin_tol;
+  size_t max_nonlin_iter;
+  size_t n_pressure_nodes;
+  size_t nonlin_method;
+  size_t n_nonlin_method_opts;
+  double *nonlin_method_opts;
+  size_t n_times; /**< length of times, # computed steps + 1 */
+  double *times;
+  size_t *print_steps; /**< T/F print on step */
+  size_t *load_steps; /**< T/F increment load on step */
+};
+#ifndef TYPE_SOLVER_FILE
+#define TYPE_SOLVER_FILE
+typedef struct SOLVER_FILE SOLVER_FILE;
+#endif
+
+
+enum {UNDEFINED_METHOD = -1, /**< gives overflow on assignment to nonlin_method */
+      NEWTON_METHOD = 1,
+      ARC_LENGTH_METHOD = 2,
+      AUX_ARC_LENGTH_METHOD = 3};
+
+/**
+ * Construct a solver file object and open the file.
+ *
+ * Calls abort w/ diagnostic message if the specified file cannot be
+ * opened.
+ */
+int solver_file_open(const char *filename,
+		     SOLVER_FILE **sf);
+
+/**
+ * Destroy the solver file object.
+ */
+int solver_file_close(SOLVER_FILE *sf);
+
+/**
+ * Read the solver file up to the prescribed loads and store
+ * information in SOLVER_FILE object.
+ */
+int solver_file_read_header(SOLVER_FILE *sf);
+
+/**
+ * Read a load increment from the solver file.
+ */
+int solver_file_read_load(SOLVER_FILE *sf,
+			  const size_t step,
+			  const size_t len_load,
+			  double *incr_load);
+
+/**
+ * Scan the load section of the file to the specified step. The
+ * specified loads are accumulated in accum_load and the current load
+ * increment is copied to incr_load. Does not initialize accum_load to
+ * zeros. Undefined behavior if accum_load and incr_load overlap.
+ */
+int solver_file_scan_to_step(SOLVER_FILE *sf,
+			     const size_t step,
+			     const size_t len_load,
+			     double *accum_load,
+			     double *incr_load);
+#endif /* #ifndef  */
