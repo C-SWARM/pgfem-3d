23963ffd5e3885fec46942689b8b0957473896b0
Matthew Mosby <mmosby1@nd.edu>
2014-11-24 14:21:27 +0000

    Remove extern C blocks from template header file.
    The include commands should be wrapped in extern C when used in c++ code,
    not internal to the c header files.

--- include/TEMPLATE.h
+++ include/TEMPLATE.h
@@ -3,14 +3,4 @@
 #ifndef 
 #define 
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* #ifdef __cplusplus */
-
-  /** */
-
-#ifdef __cplusplus
-}
-#endif /* #ifdef __cplusplus */
-
 #endif /* #ifndef  */
