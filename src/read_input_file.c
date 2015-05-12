C-SWARM/pgfem-3d

commit 28b293dce13aaa9b6eea6a71e499a5090fdf03c5

read one material at a time.
Modified `read_material` to only read one material at a time, allowing
for more unstructured input data. Additionally, error handling
(whether to abort etc.) is now the responsibility of the calling
function via the returned integer flag. The function `read_input_file`
now iteratively calls `read_material`. Note that the input format
remains unchanged.

This change is related to issue #4

src/read_input_file.c
@@ -69,7 +69,13 @@ int read_input_file(const PGFem3D_opt *opts,
   *sup = read_supports(in,*nn,ndim,*node);
 
   read_elem(in,*ne,*elem,*sup,opts->legacy);
-  read_material(in,*nmat,*material,opts->legacy);
+
+  for(int i=0, e=*nmat; i<e; i++){
+    if ( read_material(in,*nmat,*material,opts->legacy) ){
+      PGFEM_Abort();
+    }
+  }
+
   read_matgeom(in,*n_concentrations,*n_orient,*matgeom);
 
   /* NOTE: Node/Element loading assumes forces only in ndim

include/in.h
@@ -19,17 +19,28 @@
 #include "mesh_load.h"
 #include "supp.h"
 
+#include <stdlib.h>
+
 /* Function reads parameters of supports */
 SUPP read_supports (FILE *in,
 		    long nn,
 		    long ndofn,
 		    NODE *node);
 
-/* Function reads parameters of materials */
-void read_material (FILE *in,
-		    long nmat,
-		    MATERIAL *mater,
-		    int legacy);
+/**
+ * Read material property listing for material mat_id [0,nmat).
+ * 
+ * \param[in] in, File to read from
+ * \param[in] mat_id, Index to 'mater'
+ * \param[in,out] mater, Allocated material list
+ * \param[in] legacy, format flag for how to read the listing
+ *
+ * \return non-zero on internal error, i.e., I/O errors.
+ */
+int read_material (FILE *in,
+                   const size_t mat_id,
+                   MATERIAL *mater,
+                   const int legacy);
 
 /* Function gives stiffnesses matrix of materials */
 void read_matgeom (FILE *in,

src/in.c
@@ -121,55 +121,37 @@ SUPP read_supports (FILE *in,
   return (sup);
 }
 
-
-void read_material (FILE *in,
-		    long nmat,
-		    MATERIAL *mater,
-		    int legacy)
+int read_material (FILE *in,
+                   const size_t mat_id,
+                   MATERIAL *mater,
+                   const int legacy)
 {
-  if (PFEM_DEBUG) PGFEM_printf("[%d] reading materials.\n",1);
-  int err_rank = 0;
-  PGFEM_Error_rank(&err_rank);
-  /*
-    in    - Input file
-    nl    - Number of layers
-    nsup  - Number of supported nodes
-    ndofn - Number of degrees of freedom in one node
-    node  - Structure type of NODE
-    
-    %%%%%%%%%%%%%%%% TESTED 6.12.99 %%%%%%%%%%%%%%%%%
-  */
-  long i;
+  int err = 0;
   if (legacy) {
-    for (i=0;i<nmat;i++){
-      fscanf (in,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
-	      &mater[i].Ex,&mater[i].Ey,&mater[i].Ez,
-	      &mater[i].Gyz,&mater[i].Gxz,&mater[i].Gxy,
-	      &mater[i].nyz,&mater[i].nxz,&mater[i].nxy,
-	      &mater[i].ax,&mater[i].ay,&mater[i].az,
-	      &mater[i].sig);
-      mater[i].devPotFlag = mater[i].volPotFlag = 0;
-    }
+    fscanf (in,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
+            &mater[mat_id].Ex,&mater[mat_id].Ey,&mater[mat_id].Ez,
+            &mater[mat_id].Gyz,&mater[mat_id].Gxz,&mater[mat_id].Gxy,
+            &mater[mat_id].nyz,&mater[mat_id].nxz,&mater[mat_id].nxy,
+            &mater[mat_id].ax,&mater[mat_id].ay,&mater[mat_id].az,
+            &mater[mat_id].sig);
+    mater[mat_id].devPotFlag = mater[mat_id].volPotFlag = 0;
   } else {
-    for (i=0;i<nmat;i++){
-      fscanf (in,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %d %d",
-	      &mater[i].Ex,&mater[i].Ey,&mater[i].Ez,
-	      &mater[i].Gyz,&mater[i].Gxz,&mater[i].Gxy,
-	      &mater[i].nyz,&mater[i].nxz,&mater[i].nxy,
-	      &mater[i].ax,&mater[i].ay,&mater[i].az,
-	      &mater[i].sig,&mater[i].devPotFlag,&mater[i].volPotFlag);
-    }
+    fscanf (in,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %d %d",
+            &mater[mat_id].Ex,&mater[mat_id].Ey,&mater[mat_id].Ez,
+            &mater[mat_id].Gyz,&mater[mat_id].Gxz,&mater[mat_id].Gxy,
+            &mater[mat_id].nyz,&mater[mat_id].nxz,&mater[mat_id].nxy,
+            &mater[mat_id].ax,&mater[mat_id].ay,&mater[mat_id].az,
+            &mater[mat_id].sig,&mater[mat_id].devPotFlag,&mater[mat_id].volPotFlag);
   }
 
   if(ferror(in)){
-    PGFEM_printerr("[%d]ERROR:fscanf returned error"
-	    " reading material properties!\n",err_rank);
-    PGFEM_Abort();
+    PGFEM_printerr("ERROR: fscanf returned error in: %s(%s)\n",__func__,__FILE__);
+    err++;
   } else if(feof(in)){
-    PGFEM_printerr("[%d]ERROR:prematurely reached end of input file!\n",
-	    err_rank);
-    PGFEM_Abort();
+    PGFEM_printerr("ERROR: prematurely reached end of file in %s(%s)\n",__func__,__FILE__);
+    err++;
   }
+  return err;
 }
 
 void read_matgeom (FILE *in,

commit 793508054d3185e49203e6fde055cddbfbd6ab68

fix bug introduced in commit (28b293d), passing # materials instead of index.

src/read_input_file.c
@@ -71,7 +71,7 @@ int read_input_file(const PGFem3D_opt *opts,
   read_elem(in,*ne,*elem,*sup,opts->legacy);
 
   for(int i=0, e=*nmat; i<e; i++){
-    if ( read_material(in,*nmat,*material,opts->legacy) ){
+    if ( read_material(in,i,*material,opts->legacy) ){
       PGFEM_Abort();
     }
   }