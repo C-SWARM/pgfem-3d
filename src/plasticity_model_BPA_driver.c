fab2460a64a3f57a3dec7c6376495a44c9bfe28d
Matthew Mosby <mmosby1@nd.edu>
2015-08-16 14:22:32 -0400

    output response from BPA driver to bpa.dat

--- src/plasticity_model_BPA_driver.c
+++ src/plasticity_model_BPA_driver.c
@@ -11,6 +11,11 @@
 #include "_plasticity_model_BPA.h"
 #include "constitutive_model.h"
 #include "hommat.h"
+#include "utils.h"
+#include "data_structure_c.h"
+#include "index_macros.h"
+
+Define_Matrix(double);
 
 void set_mat_values(HOMMAT *m)
 {
@@ -37,15 +42,57 @@ void get_F(const double t,
            const double nu,
            double *F)
 {
+  const double rate = 0.001;
   memset(F, 0, 9*sizeof(*F));
   /* compression */
-  F[0] = F[4] = 1 + nu * t;
-  F[8] = 1 - t;
+  F[0] = F[4] = 1 + nu * rate * t;
+  F[8] = 1 - rate * t;
+}
+
+int compute_stress(double * restrict sig,
+                   const Constitutive_model *m,
+                   const void *ctx)
+{
+  int err = 0;
+  const HOMMAT *mat = m->param->p_hmat;
+  const double kappa = (2* mat->G * (1 + mat->nu)) / (3 * (1 - 2 * mat->nu));
+  double p = 0;
+  m->param->compute_dudj(m,ctx,&p);
+  p *= kappa / 2.0 ;
+  Matrix_double S;
+  Matrix_construct_redim(double,S,3,3);
+  m->param->compute_dev_stress(m,ctx,&S);
+
+  /* push stress forward */
+  const double *Fe = m->vars.Fs[2].m_pdata;
+  const double J = det3x3(Fe);
+  memset(sig,0,9*sizeof(*sig));
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 3; k++) {
+        for (int l = 0; l < 3; l++) {
+          sig[idx_2(i,j)] += Fe[idx_2(i,k)] * S.m_pdata[idx_2(k,l)] * Fe[idx_2(j,l)] / J;
+        }
+      }
+    }
+  }
+  sig[0] += p;
+  sig[4] += p;
+  sig[8] += p;
+  return err;
+}
+
+void write_data_point(FILE *f,
+                      const double *sig,
+                      const double t)
+{
+  fprintf(f,"%e\t%e\n",t,-sig[8]);
 }
 
 int main(int argc, char **argv)
 {
   int err = 0;
+  FILE *out = fopen("bpa.dat","w");
   HOMMAT *mat = malloc(sizeof(*mat));
   Model_parameters *p = malloc(sizeof(*p));
   Constitutive_model *m = malloc(sizeof(*m));
@@ -64,16 +111,24 @@ int main(int argc, char **argv)
   err += model_var_info_print(stdout,info);
   err += model_var_info_destroy(&info);
 
-  const double dt = 0.001;
-  const int nstep = 1;
+  const double dt = 1.0;
+  const int nstep = 100;
   double t = dt;
   double F[9] = {};
+  double sig[9] = {};
   void *ctx = NULL;
+
+  write_data_point(out,sig,0);
   for (int i = 0; i < nstep; i++) {
+    printf("STEP [%d]=================================\n",i);
     get_F(t,mat->nu,F);
     err += plasticity_model_BPA_ctx_build(&ctx,F,dt);
     err += m->param->integration_algorithm(m,ctx);
+    err += m->param->update_state_vars(m);
+    err += compute_stress(sig,m,ctx);
     err += plasticity_model_BPA_ctx_destroy(&ctx);
+    printf("\n");
+    write_data_point(out,sig,t);
     t += dt;
   }
 
@@ -85,5 +140,6 @@ int main(int argc, char **argv)
   free(mat);
   free(p);
   free(m);
+  fclose(out);
   return err;
 }
