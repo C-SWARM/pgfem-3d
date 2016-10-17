1537618d7823478f3361f352a9c6bb3d20edc438
Sangmin Lee <slee43@nd.edu>
2016-10-17 05:04:03 -0400

    update Makefile for verification tests

--- UQ_study/src/get_max_disp.c
+++ UQ_study/src/get_max_disp.c
@@ -221,10 +221,12 @@ int main(int argc,char *argv[])
   Model_parameters *param_list = NULL;   
   
   int in_err = 0;
+  int physicsno = 1;
+  int ndim = 3;  
   in_err = read_input_file(&options,mpi_comm,&nn,&Gnn,&ndofn,
          &ne,&ni,&err,&limit,&nmat,&nc,&np,&node,
          &elem,&mater,&matgeom,&sup,&nln,&znod,
-         &nle_s,&zele_s,&nle_v,&zele_v);
+         &nle_s,&zele_s,&nle_v,&zele_v,physicsno,&ndim);
   if(in_err){
     PGFEM_printerr("[%d]ERROR: incorrectly formatted input file!\n",
       myrank);
