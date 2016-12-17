49b6177bf64bcf43de1133177fe1890ddd9caefd
Kamal Saha <ksaha@nd.edu>
2016-12-16 19:09:50 -0500

    moving two more files to .cc

--- src/constitutive_model.cc
+++ src/constitutive_model.cc
@@ -41,9 +41,9 @@ Define_Matrix(double);
 /* add the macroscopic deformation gradient to the _TOTAL_ deformation
    gradient */
 static void cm_add_macro_F(const SUPP sup,
-                           double * restrict F)
+                           double *F)
 {
-  const double * restrict F0 = sup->F0;
+  const double *F0 = sup->F0;
   for (int i = 0; i < TENSOR_LEN; i++) F[i] += F0[i];
 }
 
