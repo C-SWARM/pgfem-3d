e6874daf175b1fb150c1f8320d42eb2c63131a12
Ivan Viti <iviti@nd.edu>
2017-05-08 11:30:30 -0400

    Update microscale_information.cc

--- src/microscale_information.cc
+++ src/microscale_information.cc
@@ -456,8 +456,8 @@ static void initialize_COMMON_MICROSCALE(COMMON_MICROSCALE *common)
 static void build_COMMON_MICROSCALE(const PGFem3D_opt *opts,
 				    MPI_Comm mpi_comm,
 				    COMMON_MICROSCALE *common, 
-            const int mp_id,
-            const Comm_hints *hints)
+				    const int mp_id,
+				    const Comm_hints *hints)
 {
   int myrank = 0;
   int nproc = 0;
