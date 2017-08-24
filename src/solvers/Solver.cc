5b4af1e77d9505811efeac5649a293eee31532fb
Luke D'Alessandro <ldalessa@uw.edu>
2017-08-24 12:42:30 -0700

    Begin solver abstraction layer.
    This sets up the build infrastructure for a `solvers` convenience library in the
    source, and moves the current `SOLVER_OPTIONS` into a new Solvers.h header with
    a new name. We add a `using SOLVER_OPTIONS = pgfem3d::Solver` definition in the
    existing header to limit the exposure of this change for now.

---
+++ src/solvers/Solver.cc
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+/// This file provides a home for the Solver abstract class' vtable, as well as
+/// the constructor dispatch functionality.
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "pgfem3d/Solver.h"
+
+pgfem3d::Solver::~Solver() {
+}
