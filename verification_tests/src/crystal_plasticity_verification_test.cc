186a7405ce1eaab3c80b25475187467a7263fefa
Sangmin Lee <slee43@nd.edu>
2017-09-15 11:38:39 -0400

    add single crystal plasticity integration point test in verification_tests/src/crystal_plasticity_verification_test.cc

--- verification_tests/src/crystal_plasticity_verification_test.cc
+++ verification_tests/src/crystal_plasticity_verification_test.cc
@@ -2,6 +2,7 @@
 # include "config.h"
 #endif
 
+#include "crystal_plasticity_integration.h"
 #include "PGFem3D_data_structure.h"
 #include "PGFem3D_to_VTK.hpp"
 #include "allocation.h"
@@ -18,17 +19,146 @@
 #include <ttl/Library/matrix.h>
 #include <cmath>
 
-namespace {
+#define DIM_3       3
+
 using namespace ttl;
-const constexpr Index<'i'> i;
-const constexpr Index<'j'> j;
-const constexpr Index<'k'> k;
-const constexpr Index<'l'> l;
+
+namespace {
+  const constexpr Index<'i'> i;
+  const constexpr Index<'j'> j;
+  const constexpr Index<'k'> k;
+  const constexpr Index<'l'> l;
+
+  template<class T1, class T2> int inv(T1 &A, T2 &AI)
+  {
+    int err = inverse(A.data, DIM_3, AI.data);
+    return err;
+  }
 }
 
 /*****************************************************/
 /*           BEGIN OF THE COMPUTER CODE              */
 /*****************************************************/
+
+
+void test_crystal_plasticity_single_crystal(void)
+{
+  //double lame1 = 75600.0;
+  //double lame2     = 26100.0;
+  double E = 70.0e+3;
+  double nu = 0.25;
+  
+  double gamma_dot_0 = 1.0;
+  double gamma_dot_s = 50.0e+9;
+  double m           = 0.05;  
+  double g0          = 210.0;
+  double G0          = 200.0;
+  double gs_0        = 330.0;
+  double w           = 0.005;
+  
+  int max_itr_stag      = 100;
+  int max_itr_hardening = 5;
+  int max_itr_M         = 100;
+  double tol_hardening  = 1.0e-4;
+  double tol_M          = 1.0e-4;
+  double computer_zero  = 1.0e-15;
+
+  // create material properties: Elasticity
+  MATERIAL_ELASTICITY mat_e;  
+  set_properties_using_E_and_nu(&mat_e,E,nu); 
+  // or you can use : set_properties_using_Lame_constants(&mat_e,lame1,lame2);
+  //print_material_property_elasticity(&mat_e); // <= this is optional
+  
+  // create slip system : 0 for FCC
+  // it is needed to setup plasticity
+  SLIP_SYSTEM slip;
+  construct_slip_system(&slip,0); 
+  
+  // create material properties: Plasticity
+  MATERIAL_CRYSTAL_PLASTICITY mat_p;
+  set_properties_crystal_plasticity(&mat_p,&slip,gamma_dot_0,gamma_dot_s, 
+                                     m,g0,G0,gs_0,w);
+  //print_material_property_crystal_plasticity(&mat_p);  // <= this is optional 
+
+  // create material plasticity: it needs material properties for elasticity and plasticity
+  MATERIAL_CONSTITUTIVE_MODEL mat;
+  set_properties_constitutive_model(&mat,&mat_e,&mat_p);
+  
+  // create solver info: criteria for numerical iterations
+  CRYSTAL_PLASTICITY_SOLVER_INFO solver_info;
+  set_crystal_plasticity_solver_info(&solver_info,max_itr_stag,
+                                                  max_itr_hardening,
+                                                  max_itr_M,
+                                                  tol_hardening,
+                                                  tol_M,
+                                                  computer_zero);  
+  //print_crystal_plasticity_solver_info(&solver_info); // <= this is optional
+  
+  // create elasticity object for integration
+  // this creates memory for stress and elasticity tensor s.t. requres destructor
+  ELASTICITY elast;
+  construct_elasticity(&elast, &mat_e, 1);  
+
+  // set variables for integration
+  ttl::Tensor<2,DIM_3,double> M,MI,pFn,pFnp1,pFnp1_I,eFnp1,Fn,Fnp1,L={},sigma,PK2dev,sigma_dev;
+  
+  pFn   = ttl::identity(i,j);
+  pFnp1 = ttl::identity(i,j);
+  eFnp1 = ttl::identity(i,j);
+  Fn    = ttl::identity(i,j);
+  Fnp1  = ttl::identity(i,j);
+      
+  double g_n,g_np1;
+  g_n = g_np1 = mat_p.g0;
+  
+  double dt = 0.001;
+  
+  double d = 1.0;
+  // set velocity gradient  
+  L[0][0] = -d;
+  L[1][1] = L[2][2] = d/2;  
+  
+  // start integration  
+  ttl::Tensor<2,DIM_3,double*> PK2(elast.S);
+  
+  FILE *fp = fopen("single_crystal_results.txt", "w");
+  
+  for(int a = 1; a<=1000; a++)
+  {
+    double lambda = 0.0;
+    double t = a*dt;
+    
+    // compute total deformation gradient using velocity gradient
+    Fnp1_Implicit(Fnp1.data, Fn.data, L.data, dt); 
+    
+    staggered_Newton_Rapson(pFnp1.data,M.data, &g_np1, &lambda, 
+                            pFn.data, Fn.data,Fnp1.data, 
+                            g_n, dt, &mat, &elast, &solver_info);
+                            
+    pFn = pFnp1(i,j);
+    Fn  = Fnp1(i,j);
+    inv(pFnp1,pFnp1_I);
+    eFnp1 = Fnp1(i,k)*pFnp1_I(k,j);
+       
+    g_n = g_np1;
+    
+    
+    // print result at time t
+    double sigma_eff;
+    double PK2_eff;
+    double det_pF = ttl::det(pFnp1);
+        
+    elast.update_elasticity(&elast,eFnp1.data,0);
+    elast.compute_PK2_eff(&elast,&PK2_eff);
+    elast.compute_Cauchy_eff(&elast,&sigma_eff,eFnp1.data);   
+
+    fprintf(fp, "%e %e %e %e %e %e\n",t,sigma_eff,PK2_eff, g_np1, det_pF, PK2[0][0]);
+  }    
+  
+  fclose(fp);  
+  destruct_elasticity(&elast);
+  destruct_slip_system(&slip);
+}
 
 
 int main(int argc,char *argv[])
@@ -145,7 +275,7 @@ int main(int argc,char *argv[])
   double temp[6];
   if(myrank==0)
   {
-    constitutive_model_test(hommat, NULL, -1); // F(t)
+    test_crystal_plasticity_single_crystal(); // F(t)
     FILE *fp_r = fopen("single_crystal_results.txt", "r");
     for(int a=0; a<1000; a++)
     {
