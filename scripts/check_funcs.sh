ed2f179f8e5281ee6a162719cc02cfe9c75ee539
Ezra Kissel <ezkissel@indiana.edu>
2017-11-01 16:09:13 -0400

    Update URL to dedicated CSWARM web server for scan-builds output.

--- scripts/check_funcs.sh
+++ scripts/check_funcs.sh
@@ -13,7 +13,7 @@ function check_scan() {
     is_file $BOUT || return
     if grep -q -E "[0-9]+ (bug|bugs) found" $BOUT; then
        DIR=`tail -n 1 $BOUT | egrep -o "[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]"`;
-       echo; echo "View report at http://`hostname -f`/scan-builds/$DIR";
+       echo; echo "View report at https://gitlab-web-cswarm.crc.nd.edu/$DIR";
        return 2;
     fi
 }
