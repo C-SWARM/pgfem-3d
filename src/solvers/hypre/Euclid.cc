3f0701d92f94ac5b5399152f661a1a2f6b6158c7
ahowell3 <ahowell3@nd.edu>
2018-04-06 12:24:28 -0400

    Revert "revised hypre Euclid parameters"
    This reverts commit fb385df226ab0174fdc14b253b22d45f9bec8fbf.

--- src/solvers/hypre/Euclid.cc
+++ src/solvers/hypre/Euclid.cc
@@ -10,24 +10,19 @@ using pgfem3d::solvers::hypre::Euclid;
 
 /// Defines the way we configure the Euclid preconditioner.
 static const char *config[] = {
- "-level", 
- "1",               //factorization level for ILU(k): 1 (default, we use) varies up to 8
- "-bj", 
- "1",               //parallelization method: 0 (false = PILU, default), 1 (true = BJ-ILU, we use)
- "-rowScale", 
- "0",               //scale values prior to factorization: 0 (false, default), 1 (true, good for large/complex system)
- "-ilut", 
- "0",               //factorization method: 0 (faluse = ILU(k), default, we use), 1 (true = ILUT)
- "-sparseA", 
- "0",               //drop-tolerance for ILU(k) factorizaion: 0 (false, default, we use), 1 (true)
- "-eu_stats", 
- "0"                //print runtime parameters and compute time info: 0 (false, default); 1 (true)
+  "-bj",
+  "-rowScale",
+  "-maxNzPerRow",
+  "100",
+  "-level",
+  "1",
+  ""
 };
 
 Euclid::Euclid(MPI_Comm comm) : Preconditioner(comm)
 {
   if (HYPRE_EuclidCreate(_comm, &_solver) or
-      HYPRE_EuclidSetParams(_solver, 12, const_cast<char**>(config))) {
+      HYPRE_EuclidSetParams(_solver, 7, const_cast<char**>(config))) {
     throw std::runtime_error("failed to initialize Euclid preconditioner");
   }
 }
@@ -49,7 +44,7 @@ Euclid::reset()
 {
   HYPRE_EuclidDestroy(_solver);
   if (HYPRE_EuclidCreate(_comm, &_solver) or
-      HYPRE_EuclidSetParams(_solver, 12, const_cast<char**>(config))) {
+      HYPRE_EuclidSetParams(_solver, 7, const_cast<char**>(config))) {
     throw std::runtime_error("failed to initialize Euclid preconditioner");
   }
 }
