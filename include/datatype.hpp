c6ebb090562e0931e85fb6057c9b0b2273504f89
Kamal Krishna Saha <ksaha@nd.edu>
2019-04-26 08:16:25 -0400

    forgot to add the header file datatype.hpp with the pervious commit

---
+++ include/datatype.hpp
@@ -0,0 +1,10 @@
+#ifndef PGFEM3D_DATATYPE
+#define PGFEM3D_DATATYPE
+
+//using Ai_t = long;
+//using HYPRE_t = long long;
+//typedef long Ai_t;  
+#define Ai_t long
+#define HYPRE_t long long
+
+#endif
