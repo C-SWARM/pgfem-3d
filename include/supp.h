C-SWARM/pgfem-3d

commit 80257c67e5328a65d8d0cccab2afbcce6be8e37e

Fix a possible regression when reading Dirischlet BC.
The function that reads the boundary conditions is creating a list of boundary
nodes that have a prescribed deflection, however it looks like this list wasn't
being accurately filled. This commit correctly builds the list of prescribed
deflection.

include/supp.h
@@ -5,37 +5,31 @@
 
 /** Structure of support nodes */
 struct SUPP_1 {
-  /**
-     nfn - number of free nodes
-     nsn - number of supported nodes
-     ndn - number of nodes with prescribed deflection
-     npd - number of magnidudes of prescribed deflection
-     nde - number of elements with prescribed deflection
-     nd_be - number of bounding elements w/ pre. def.
-  */
-  long nfn,nsn,ndn,npd,nde,nd_be;
-  /**
-     free - list of free nodes
-     supp - list of supported nodes
-     lnpd - list of nodes with prescribed deflection
-     lepd - list of elements w/ pre. def
-     lbepd - -- " -- bounding elements ---"---
-  */
-  long *free,*supp,*lnpd,*lepd,*lbepd;
-  /** defl - value of deflection at the node. */
-  double *defl;
+  long       nfn;             //!< number of free nodes
+  long       nsn;             //!< number of supported nodes
+  long       ndn;             //!< number of nodes with prescribed deflection
+  long       npd;             //!< number of magnidudes of prescribed deflection
+  long       nde;             //!< number of elements with prescribed deflection
+  long     nd_be;             //!< number of bounding elements w/ pre. def.
+  long     *free;             //!< list of free nodes
+  long     *supp;             //!< list of supported nodes
+  long     *lnpd;             //!< list of nodes with prescribed deflection
+  long     *lepd;             //!< list of elements w/ pre. def
+  logn    *lbepd;             //!< list of bounding elements
+  double   *defl;             //!< value of deflection at the node.
   double *defl_d;
 
-  /** multiscale information. For interfaces, the displacement jump
-      vector is computed from the first six (6) prescribed
-      displacements */
-  double *F0; /**< macroscale deformation gradient contribution */
-  double *N0; /**< macroscale unit normal */
-  double lc; /**< multiscale scaling parameter. For interfaces, is the
-                thickness of the unit cell/interface layer. */
-  double v0; /**< Volume of microstructure V = B U H [Miehe 2002]*/
-  int multi_scale; /**< flag for whether or not multiscale routines
-                      should be executed. */
+  /// Multiscale information. For interfaces, the displacement jump vector is
+  /// computed from the first six (6) prescribed displacements
+  /// @{
+  double *F0;          //!< macroscale deformation gradient contribution
+  double *N0;          //!< macroscale unit normal
+  double  lc;          //!< multiscale scaling parameter. For interfaces, is the
+                       //   thickness of the unit cell/interface layer.
+  double  v0;          //!< Volume of microstructure V = B U H [Miehe 2002]
+  int multi_scale;     //!< flag for whether or not multiscale routines should
+                       //   be executed.
+  /// @}
 };
 
 using SUPP = SUPP_1*;

src/in.cc
@@ -34,41 +34,51 @@ SUPP read_Dirichlet_BCs(FILE *in,
 {
   int err_rank = 0;
   PGFEM_Error_rank(&err_rank);
-  if (PFEM_DEBUG) PGFEM_printf("[%d] reading supports.\n",err_rank);
-  long n,pom;
+  if (PFEM_DEBUG) {
+    PGFEM_printf("[%d] reading supports.\n", err_rank);
+  }
+
+  // Return 1 if the node with id `n` has a prescribed deflection.
+  auto has_prescribed_deflection = [ndofn,node,mp_id](auto n) {
+    auto& ids = node[n].id_map[mp_id].id;
+    for (int k = 0; k < ndofn; ++k) {
+      if (ids[k] == 1 || ids[k] < 0) {
+        return 1;
+      }
+    }
+    return 0;
+  };
+
   SUPP sup = PGFEM_calloc (SUPP_1, 1);
 
   // read supported nodes
-
-  if(in==NULL)
-    sup->nsn = 0;
-  else
+  if (in) {
     CHECK_SCANF(in, "%ld", &sup->nsn);
+  }
 
-  if(sup->nsn == 0)  sup->supp = PGFEM_calloc (long, 1);
-  else               sup->supp = PGFEM_calloc (long, sup->nsn);
+  if (sup->nsn == 0)  {
+    sup->supp = PGFEM_calloc (long, 1);
+  }
+  else {
+    sup->supp = PGFEM_calloc (long, sup->nsn);
+  }
 
-  for(int i=0; i<sup->nsn; i++)
-  {
-    CHECK_SCANF(in,"%ld",&n);
+  for (int i = 0, e = sup->nsn; i < e; ++i) {
+    long n;
+    CHECK_SCANF(in,"%ld", &n);
     sup->supp[i] = n;
-
-    pom = 0;
-    for(int k=0; k<ndofn; k++)
-    {
-      CHECK_SCANF(in,"%ld",&node[n].id_map[mp_id].id[k]);
-      if((node[n].id_map[mp_id].id[k] == 1 || node[n].id_map[mp_id].id[k] <= -1) && pom == 0) {
-        sup->ndn++; pom = 1;
-      }
+    for (int k = 0; k < ndofn; ++k) {
+      CHECK_SCANF(in, "%ld", &node[n].id_map[mp_id].id[k]);
     }
-    if(ferror(in))
-    {
-      PGFEM_printerr("[%d]ERROR:fscanf returned error"
-                     " reading support %ld!\n",err_rank,i);
+    sup->ndn += has_prescribed_deflection(n);
+
+    if (ferror(in)) {
+      PGFEM_printerr("[%d]ERROR:fscanf returned error reading support %ld!\n",
+                     err_rank, i);
       PGFEM_Abort();
     }
-    else if(feof(in))
-    {
+
+    if (feof(in)) {
       PGFEM_printerr("[%d]ERROR:prematurely reached end of input file!\n",
                      err_rank);
       PGFEM_Abort();
@@ -78,19 +88,20 @@ SUPP read_Dirichlet_BCs(FILE *in,
   /***************************************************/
   /* create list of nodes with prescribed deflection */
   /***************************************************/
+  if (sup->ndn == 0) {
+    sup->lnpd = PGFEM_calloc (long, 1);
+  }
+  else {
+    sup->lnpd = PGFEM_calloc (long, sup->ndn);
+  }
 
-  if (sup->ndn == 0)  sup->lnpd = PGFEM_calloc (long, 1);
-  else                sup->lnpd = PGFEM_calloc (long, sup->ndn);
-
-  int ii = 0;
-  for(int i=0; i<sup->nsn; i++)
-  {
-    for(int k=0; k<ndofn; k++)
-    {
-      if(node[n].id_map[mp_id].id[k] == 1 || node[n].id_map[mp_id].id[k] <= -1) {
-        sup->lnpd[ii] = sup->supp[i];
-        ii++;  break;
-      }
+  // Copy the list of nodes with prescribed deflection from `sup->supp` to
+  // `sup->lnpd`.
+  for (int i = 0, insert = 0, e = sup->nsn; i < e; ++i) {
+    long n = sup->supp[i];
+    if (has_prescribed_deflection(n)) {
+      assert(insert < sup->ndn);
+      sup->lnpd[insert++] = n;
     }
   }
 
@@ -98,7 +109,7 @@ SUPP read_Dirichlet_BCs(FILE *in,
   sup->F0 = PGFEM_calloc(double, 9);
   sup->N0 = PGFEM_calloc(double, 3);
 
-  return (sup);
+  return sup;
 }
 
 /// read Dirichlet boundary condition values
@@ -168,9 +179,11 @@ SUPP read_supports(FILE *in,
   %%%%%%%%%%%%%%%% TESTED 6.12.99 %%%%%%%%%%%%%%%%%
 */
 {
-  SUPP sup = read_Dirichlet_BCs(in,nn,ndofn,node,mp_id);
-  read_Dirichlet_BCs_values(in,nn,ndofn,node,sup,mp_id);
-
+  SUPP sup = read_Dirichlet_BCs(in, nn, ndofn, node, mp_id);
+  if (read_Dirichlet_BCs_values(in, nn, ndofn, node, sup, mp_id)) {
+    PGFEM_printf("Error reading Dirichlet boundary conditions");
+    PGFEM_Abort();
+  }
   return sup;
 }
 

commit 1a94d483d6068a9f4589f0ee1fd18c2d44d17682

Fix regression introduced in 80257c67.

include/supp.h
@@ -15,7 +15,7 @@ struct SUPP_1 {
   long     *supp;             //!< list of supported nodes
   long     *lnpd;             //!< list of nodes with prescribed deflection
   long     *lepd;             //!< list of elements w/ pre. def
-  logn    *lbepd;             //!< list of bounding elements
+  long    *lbepd;             //!< list of bounding elements
   double   *defl;             //!< value of deflection at the node.
   double *defl_d;
 