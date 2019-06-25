a171240f3c00d304c844ab1c7d19779397739cc2
Luke D'Alessandro <ldalessa@uw.edu>
2019-06-25 11:25:16 -0700

    Make sure the BPA plasticity model's `compute_dMdu` matches the base class virtual function type.

--- include/plasticity_model_BPA.h
+++ include/plasticity_model_BPA.h
@@ -46,17 +46,17 @@ class BPA_PARAM: public Model_parameters
                                 double *L,
                                 double *S,
                                 const bool compute_stiffness) const;
-  virtual int update_state_vars(Constitutive_model *m) const;  
+  virtual int update_state_vars(Constitutive_model *m) const;
   virtual int reset_state_vars(Constitutive_model *m) const;
   virtual int get_var_info(Model_var_info &info) const;
-  virtual int get_F(const Constitutive_model *m, 
+  virtual int get_F(const Constitutive_model *m,
                     double *F,
                     const int stepno) const;
-  virtual int get_pF(const Constitutive_model *m, 
-                     double *F, 
+  virtual int get_pF(const Constitutive_model *m,
+                     double *F,
                      const int stepno) const;
-  virtual int get_eF(const Constitutive_model *m, 
-                     double *F, 
+  virtual int get_eF(const Constitutive_model *m,
+                     double *F,
                      const int stepno) const;
   virtual int get_hardening(const Constitutive_model *m,
                             double *var,
@@ -64,14 +64,14 @@ class BPA_PARAM: public Model_parameters
   virtual int get_plast_strain_var(const Constitutive_model *m,
                                    double *lam_p)
   const { return cm_get_eq_plastic_strain(m, lam_p);};
-                              
+
   virtual int write_restart(FILE *fp,
                             const Constitutive_model *m) const;
   virtual int read_restart(FILE *fp,
                            Constitutive_model *m) const;
   virtual int compute_dMdu(const Constitutive_model *m,
                            CM_Ctx &cm_ctx,
-                           const double *Grad_op,
+                           double *Grad_op,
                            const int nne,
                            const int ndofn,
                            double *dM_du) const;
